976fe132b11a54991d30844e2e76ac60f26a1eff
= <kjussila@mac.com>
2025-04-21 20:23:07 -0500

    Created spider enemy, implemented animation player

--- scripts/utils/health.gd
+++ scripts/utils/health.gd
@@ -5,15 +5,43 @@ signal health_changed (new_health: int)
 @export var max_health = 100
 var health = max_health
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time
 func _ready() -> void:
 	health = max_health
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func take_damage(damage: int, negative: bool = false) -> int:
+	# Return current health if no damage is taken
+	if not damage: return health
 
-func take_damage(damage: int) -> int:
-	health = max(0, health - damage)
+	# Deal damage
+	if negative:
+		health = health - damage
+	else:
+		health = max(0, health - damage)
+	
+	# Emit health change signal and return new health
+	emit_signal("health_changed", health)
+	return health
+
+func heal(amount: int, overheal: bool = false) -> int:
+	# Return current health if no healing is done
+	if not amount: return health
+
+	# Heal
+	if overheal:
+		health = health + amount
+	else:
+		health = min(max_health, health + amount)
+	
+	# Emit health change signal and return new health
+	emit_signal("health_changed", health)
+	return health
+
+func set_health(new_health: int) -> int:
+	# Return current health if no change is made
+	if new_health == health: return health
+
+	# Update health and emit signal
+	health = new_health
 	emit_signal("health_changed", health)
 	return health
